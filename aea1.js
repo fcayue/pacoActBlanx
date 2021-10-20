83b8b0cae8352c10984e9e2bce3531a1072e1915
paco <pacocayuela13@gmail.com>
2021-10-20 16:49:54 +0200

    First commit

---
+++ aea1.js
@@ -0,0 +1,21 @@
+var fs = require('fs'); 
+
+process.argv.forEach(function (val, index, array) {
+	//console.log(index + ': ' + val);
+});
+
+var fileName = process.argv[2];
+var fileContent = process.argv[3]
+
+auxPos = fileName.indexOf("=");
+fileName = fileName.substr(auxPos+1);
+
+auxPos = fileContent.indexOf("=");
+fileContent = fileContent.substr(auxPos+1);
+
+
+
+fs.writeFile(fileName,fileContent, (err) => {
+	if(err) return err;
+	console.log("The file has been saved");
+});process.argv[3]
